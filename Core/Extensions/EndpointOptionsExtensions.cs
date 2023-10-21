dfee0401acd29a9710d737a36e525f070dc3ffe8
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-10-21 18:42:19 +0200

    + 'IdentityServer4' project as 'Core'

---
+++ Core/Extensions/EndpointOptionsExtensions.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdNet8.Configuration;
+using IdNet8.Hosting;
+using static IdNet8.Constants;
+
+namespace IdNet8.Extensions
+{
+    internal static class EndpointOptionsExtensions
+    {
+        public static bool IsEndpointEnabled(this EndpointsOptions options, Endpoint endpoint)
+        {
+            return endpoint?.Name switch
+            {
+                EndpointNames.Authorize => options.EnableAuthorizeEndpoint,
+                EndpointNames.CheckSession => options.EnableCheckSessionEndpoint,
+                EndpointNames.DeviceAuthorization => options.EnableDeviceAuthorizationEndpoint,
+                EndpointNames.Discovery => options.EnableDiscoveryEndpoint,
+                EndpointNames.EndSession => options.EnableEndSessionEndpoint,
+                EndpointNames.Introspection => options.EnableIntrospectionEndpoint,
+                EndpointNames.Revocation => options.EnableTokenRevocationEndpoint,
+                EndpointNames.Token => options.EnableTokenEndpoint,
+                EndpointNames.UserInfo => options.EnableUserInfoEndpoint,
+                _ => true
+            };
+        }
+    }
+}
