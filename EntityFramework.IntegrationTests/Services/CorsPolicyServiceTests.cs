8a5d0953a90cdcfacdd2174dfedbac8a68a974c3
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-10-21 20:27:17 +0200

    + 'EntityFramework'

---
+++ EntityFramework.IntegrationTests/Services/CorsPolicyServiceTests.cs
@@ -0,0 +1,126 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using System;
+using System.Collections.Generic;
+using IdNet8.EntityFramework.DbContexts;
+using IdNet8.EntityFramework.Mappers;
+using IdNet8.Models;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using System.Linq;
+using IdNet8.EntityFramework.Options;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using IdNet8.EntityFramework.Interfaces;
+using IdNet8.EntityFramework.Services;
+
+namespace IdNet8.EntityFramework.IntegrationTests.Services
+{
+    public class CorsPolicyServiceTests : IntegrationTest<CorsPolicyServiceTests, ConfigurationDbContext, ConfigurationStoreOptions>
+    {
+        public CorsPolicyServiceTests(DatabaseProviderFixture<ConfigurationDbContext> fixture) : base(fixture)
+        {
+            foreach (var options in TestDatabaseProviders.SelectMany(x => x.Select(y => (DbContextOptions<ConfigurationDbContext>)y)).ToList())
+            {
+                using (var context = new ConfigurationDbContext(options, StoreOptions))
+                    context.Database.EnsureCreated();
+            }
+        }
+
+        [Theory, MemberData(nameof(TestDatabaseProviders))]
+        public void IsOriginAllowedAsync_WhenOriginIsAllowed_ExpectTrue(DbContextOptions<ConfigurationDbContext> options)
+        {
+            const string testCorsOrigin = "https://identityserver.io/";
+
+            using (var context = new ConfigurationDbContext(options, StoreOptions))
+            {
+                context.Clients.Add(new Client
+                {
+                    AllowedIdentityTokenSigningAlgorithms = new List<string>() {"a"},
+                    BackChannelLogoutUri = "",
+                    ClientUri = "",
+                    Description = "",
+                    FrontChannelLogoutUri = "",
+                    LogoUri = "",
+                    PairWiseSubjectSalt = "",
+                    UserCodeType = "",
+                    ClientId = Guid.NewGuid().ToString(),
+                    ClientName = Guid.NewGuid().ToString(),
+                    AllowedCorsOrigins = new List<string> { "https://www.identityserver.com" }
+                }.ToEntity());
+                context.Clients.Add(new Client
+                {
+                    AllowedIdentityTokenSigningAlgorithms = new List<string>() {"a"},
+                    BackChannelLogoutUri = "",
+                    ClientUri = "",
+                    Description = "",
+                    FrontChannelLogoutUri = "",
+                    LogoUri = "",
+                    PairWiseSubjectSalt = "",
+                    UserCodeType = "",
+                    ClientId = "2",
+                    ClientName = "2",
+                    AllowedCorsOrigins = new List<string> { "https://www.identityserver.com", testCorsOrigin }
+                }.ToEntity());
+                context.SaveChanges();
+            }
+
+            bool result;
+            using (var context = new ConfigurationDbContext(options, StoreOptions))
+            {
+                var ctx = new DefaultHttpContext();
+                var svcs = new ServiceCollection();
+                svcs.AddSingleton<IConfigurationDbContext>(context);
+                ctx.RequestServices = svcs.BuildServiceProvider();
+                var ctxAccessor = new HttpContextAccessor();
+                ctxAccessor.HttpContext = ctx;
+
+                var service = new CorsPolicyService(ctxAccessor, FakeLogger<CorsPolicyService>.Create());
+                result = service.IsOriginAllowedAsync(testCorsOrigin).Result;
+            }
+
+            Assert.True(result);
+        }
+
+        [Theory, MemberData(nameof(TestDatabaseProviders))]
+        public void IsOriginAllowedAsync_WhenOriginIsNotAllowed_ExpectFalse(DbContextOptions<ConfigurationDbContext> options)
+        {
+            using (var context = new ConfigurationDbContext(options, StoreOptions))
+            {
+                context.Clients.Add(new Client
+                {
+                    AllowedIdentityTokenSigningAlgorithms = new List<string>() {"a"},
+                    BackChannelLogoutUri = "",
+                    ClientUri = "",
+                    Description = "",
+                    FrontChannelLogoutUri = "",
+                    LogoUri = "",
+                    PairWiseSubjectSalt = "",
+                    UserCodeType = "",
+                    ClientId = Guid.NewGuid().ToString(),
+                    ClientName = Guid.NewGuid().ToString(),
+                    AllowedCorsOrigins = new List<string> { "https://www.identityserver.com" }
+                }.ToEntity());
+                context.SaveChanges();
+            }
+
+            bool result;
+            using (var context = new ConfigurationDbContext(options, StoreOptions))
+            {
+                var ctx = new DefaultHttpContext();
+                var svcs = new ServiceCollection();
+                svcs.AddSingleton<IConfigurationDbContext>(context);
+                ctx.RequestServices = svcs.BuildServiceProvider();
+                var ctxAccessor = new HttpContextAccessor();
+                ctxAccessor.HttpContext = ctx;
+
+                var service = new CorsPolicyService(ctxAccessor, FakeLogger<CorsPolicyService>.Create());
+                result = service.IsOriginAllowedAsync("InvalidOrigin").Result;
+            }
+
+            Assert.False(result);
+        }
+    }
+}
